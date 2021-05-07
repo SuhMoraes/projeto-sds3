113d6fa2898c91e657ee1405b3e9e433e9d5d489
Suh Moraes <suh.alvesmoraes@gmai.com>
2021-05-07 11:15:39 -0300

    DataTable integration :card_index_dividers:

--- frontend/src/components/DataTable/index.tsx
+++ frontend/src/components/DataTable/index.tsx
@@ -1,64 +1,58 @@
-const DataTable = () => {
-  return (
-    <div className="table-responsive">
-      <table className="table table-striped table-sm">
-          <thead>
-              <tr>
-                  <th>Data</th>
-                  <th>Vendedor</th>
-                  <th>Clientes visitados</th>
-                  <th>Negócios fechados</th>
-                  <th>Valor</th>
-              </tr>
-          </thead>
-          <tbody>
-              <tr>
-                  <td>22/04/2021</td>
-                  <td>Barry Allen</td>
-                  <td>34</td>
-                  <td>25</td>
-                  <td>15017.00</td>
-              </tr>          
-              <tr>
-                  <td>22/04/2021</td>
-                  <td>Barry Allen</td>
-                  <td>34</td>
-                  <td>25</td>
-                  <td>15017.00</td>
-              </tr>         
-              <tr>
-                  <td>22/04/2021</td>
-                  <td>Barry Allen</td>
-                  <td>34</td>
-                  <td>25</td>
-                  <td>15017.00</td>
-              </tr>
-              <tr>
-                  <td>22/04/2021</td>
-                  <td>Barry Allen</td>
-                  <td>34</td>
-                  <td>25</td>
-                  <td>15017.00</td>
-              </tr>
-              <tr>
-                  <td>22/04/2021</td>
-                  <td>Barry Allen</td>
-                  <td>34</td>
-                  <td>25</td>
-                  <td>15017.00</td>
-              </tr>
-              <tr>
-                  <td>22/04/2021</td>
-                  <td>Barry Allen</td>
-                  <td>34</td>
-                  <td>25</td>
-                  <td>15017.00</td>
-              </tr>
-          </tbody>
-      </table>
-    </div>
+import axios from "axios";
+import { useEffect, useState } from "react";
+import { SalePage } from "types/sale";
+import { formatLocalDate } from "utils/format";
+import { BASE_URL } from "utils/requests";
 
-  );
+const DataTable = () => {
+
+    //Criação de um Estado
+    const [page, setPage] = useState < SalePage >({
+        first: true,
+        last: true,
+        number: 0,
+        totalElements: 0,
+        totalPages: 0
+    });
+
+    // Fazendo a chamada na API 
+    useEffect(() => {
+        axios.get(`${BASE_URL}/sales?page=1&size=20&sort=date,desc`)
+            .then((response) => {
+                setPage(response.data);
+            })
+    })
+ 
+  return (
+        <div className="table-responsive">
+            <table className="table table-striped table-sm">
+                <thead>
+                    <tr>
+                        <th>Data</th>
+                        <th>Vendedor</th>
+                        <th>Clientes visitados</th>
+                        <th>Negócios fechados</th>
+                        <th>Valor</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {page.content?.map(item => (
+                        <tr key={item.id}>
+                        <td>{formatLocalDate(item.date, "dd/yyyy")}</td>
+                        <td>{item.seller.name}</td>
+                        <td>{item.visited}</td>
+                        <td>{item.deals}</td>
+                        <td>{item.amount.toFixed(2)}</td>
+                    </tr>
+                    ))}   
+                    
+                
+                </tbody>
+            </table>
+        </div>
+
+    );
 }
 
 export default DataTable;
+
--- frontend/src/types/sale.ts
+++ frontend/src/types/sale.ts
@@ -1,4 +1,28 @@
+import { Seller } from "./seller";
+
 // Definindo um tipo
+export type Sale = {
+  id: number;
+  visited: number;
+  deals: number;
+  amount: number;
+  date: string;
+  seller: Seller;
+}
+
+export type SalePage = {
+  content?: Sale[]; //Opcional
+  last: boolean;
+  totalPages: number;
+  totalElements: number;
+  size?: number; //Opcional
+  number: number;
+  first: boolean;
+  numberOfElements?: number; //Opcional
+  empty?: boolean; //Opcional
+
+}
+
 export type SaleSum = {
   sellerName: string;
   sum: number;
