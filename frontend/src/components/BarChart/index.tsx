6848cdff9deccce1b53006b91c9313323d3f1b7f
Suh Moraes <suh.alvesmoraes@gmai.com>
2021-05-07 13:18:43 -0300

    Update file - BarChart

--- frontend/src/components/BarChart/index.tsx
+++ frontend/src/components/BarChart/index.tsx
@@ -1,7 +1,7 @@
 import axios from 'axios';
 import { useEffect, useState } from 'react';
 import Chart from 'react-apexcharts'
-import { SaleSucess, SaleSum } from 'types/sale';
+import { SaleSucess} from 'types/sale';
 import { round } from 'utils/format';
 import { BASE_URL } from 'utils/requests';
 
